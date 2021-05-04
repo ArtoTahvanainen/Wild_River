d0fe7c1f06131c3f9dfe6b3a66397097574f5927
ArtoTahvanainen <78968731+ArtoTahvanainen@users.noreply.github.com>
2021-05-04 14:43:57 +0300

    Update infoa.js

--- WildRiver/JS/infoa.js
+++ WildRiver/JS/infoa.js
@@ -4,7 +4,7 @@
 var coll = document.getElementsByClassName("avaa"); //Haetaan elementti class tunnuksella
 var i;
 
-for (i = 0; i < coll.length; i++) { //Klikkaamalla muuttuja "i" saa arvon 1
+for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     var sisaltoDIV = this.nextElementSibling; //"avaa" div alla oleva elementti ja sille muuttuja
     if (sisaltoDIV.style.display === "block") { //Jos display=block; muutetaan display=none;
